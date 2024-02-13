683d11a89aab22668eebb690f547dd72215fce88
Christina Brockway <141530991+SeeBee8@users.noreply.github.com>
2024-02-13 14:49:50 -0600

    created part A

---
+++ Sales_Streamlit.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+pd.set_option("display.max_columns", 100)
+from io import StringIO
+
+
+st.title("Sales Price Analysis")
+
+## Function to load data
+@st.cache_data
+def load_data():
+    df=pd.read_csv('data/sales_prediction_clean.csv')
+    return df
+
+# Load the dataframe
+st.header('Product Sales Data')
+df = load_data()
+st.dataframe(df, width=800)
+
+# Display descriptive statistics
+st.markdown("#### Descriptive Statistics")
+stats = st.button("Descriptive Statistics")
+if stats:
+    st.dataframe(df.describe().round(2))
+
+# Create string buffer 
+buffer = StringIO()
+#Write info into the buffer
+df.info(buf=buffer)
+summary_info = buffer.getvalue()
+st.markdown('#### Summary Info')
+summary_text = st.button('Summary Info')
+if summary_text:
+    st.text(summary_info)
+
+# Display Null values
+st.markdown('#### Null Values')
+nulls = df.isna().sum()
+null_button = st.button("Null Values")
+if null_button:
+    st.dataframe(nulls)
+
